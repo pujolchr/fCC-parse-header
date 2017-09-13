018d46af19e4da2b59ae7f511828fb6d28819963
Christian Pujol <pujolchr@gmail.com>
2017-09-13 15:54:47 +0200

    return unfilterd payload

--- parse-header.js
+++ parse-header.js
@@ -6,6 +6,7 @@ const path = require('path');
 const url = require('url');
 const http = require('http');
 const fs = require('fs');
+const dns = require('dns');
 
 if (process.argv.length !== 3) {
   process.stderr.write(`usage: ${path.basename(process.argv[1])} port\n`);
@@ -17,9 +18,16 @@ const sendResponse = (r, data) => {
   r.write(JSON.stringify(data));
   r.end();
 };
-
+ 
 const server = http.createServer((req, res) => {
-    sendResponse(res, {});
+  let payload = {};
+  console.log(req.headers);
+  payload['user-agent'] = req.headers['user-agent'];
+  payload.language = req.headers['accept-language'];
+  dns.resolve4(req.headers.host.replace(/:.*$/, ''), addr => {
+    payload.ip = addr;
+    sendResponse(res, payload);
+  });
 });
 
 server.listen(process.argv[2]);
